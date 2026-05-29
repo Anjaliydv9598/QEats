bb6a3a5855d5cd6f94d2d141752fdb5ef790153e
Anjaliydv9598 <anjaliydv9598.com>
2026-05-29 19:59:58 +0530

    Implemented JWT authentication and security configuration

--- src/main/java/com/jsp/dto/RegisterRequest.java
+++ src/main/java/com/jsp/dto/RegisterRequest.java
@@ -1,27 +1,14 @@
-package com.jsp.entity;
-
-import jakarta.persistence.*;
+package com.jsp.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-@Entity
-@Table(name="users")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+public class RegisterRequest {
     private String name;
     private String email;
     private String password;
-    private String role;
-
-
-
 }
