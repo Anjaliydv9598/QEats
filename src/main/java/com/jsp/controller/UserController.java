5a8e74ad5d83c7f4bca67365034f6f31e052e14f
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-02 16:16:34 +0530

    Update the classes

--- src/main/java/com/jsp/controller/UserController.java
+++ src/main/java/com/jsp/controller/UserController.java
@@ -1,62 +1,97 @@
 package com.jsp.controller;
 
+import com.jsp.dto.UserResponse;
 import com.jsp.entity.User;
 import com.jsp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/qeats/user")
 public class UserController {
 
-    @Autowired
-     UserService userService;
+    private final UserService userService;
 
-//     Constructor Injection
-    public UserController(UserService userService){
+    @Autowired
+     public UserController(UserService userService){
         this.userService=userService;
     }
 
-//    GET ALL USERS (Admin purpose)
-    @GetMapping
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
-    }
-
-//    get user by  id
-    @GetMapping("/{id}")
-    public User getUserById(@PathVariable Integer id){
-        return userService.getUserById(id).orElseThrow(()->new RuntimeException("User not found with id : " + id));
-
-    }
-
-//    update user
-    @PutMapping("/{id}")
-    public User updateUser(@PathVariable Integer id, @RequestBody User user ){
-        return userService.updateUser(id,user);
-    }
-
-//    delete user
-    @DeleteMapping("/{id}")
-
-    public String deleteUserById(@RequestParam Integer id){
-        userService.deleteUser(id);
-        return "User delete succesfully";
-    }
-
-//    register
     @PostMapping("/register")
-    public User registerUser(@RequestBody User user){
-        return userService.register(user);
+    public RepositoryEntity<UserResponse> registerUser(@RequestBody User user){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(userService.register(user));
     }
 
-//    login
-    @PostMapping("/login")
-    public User loginUser(@RequestBody User user){
-        return userService.login(user.getEmail(),user.getPassword());
+    @PostMapping("/register")
+    public RepositoryEntity<UserResponse> login(@RequestBody User user){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userService.login(userRequest));
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+////     Constructor Injection
+//    public UserController(UserService userService){
+//        this.userService=userService;
+//    }
+//
+////    GET ALL USERS (Admin purpose)
+//    @GetMapping
+//    public List<User> getAllUsers(){
+//        return userService.getAllUsers();
+//    }
+//
+////    get user by  id
+//    @GetMapping("/{id}")
+//    public User getUserById(@PathVariable Integer id){
+//        return userService.getUserById(id).orElseThrow(()->new RuntimeException("User not found with id : " + id));
+//
+//    }
+//
+////    update user
+//    @PutMapping("/{id}")
+//    public User updateUser(@PathVariable Integer id, @RequestBody User user ){
+//        return userService.updateUser(id,user);
+//    }
+//
+////    delete user
+//    @DeleteMapping("/{id}")
+//
+//    public String deleteUserById(@RequestParam Integer id){
+//        userService.deleteUser(id);
+//        return "User delete succesfully";
+//    }
+//
+////    register
+//    @PostMapping("/register")
+//    public User registerUser(@RequestBody User user){
+//        return userService.register(user);
+//    }
+//
+////    login
+//    @PostMapping("/login")
+//    public User loginUser(@RequestBody User user){
+//        return userService.login(user.getEmail(),user.getPassword());
+//    }
 
 
 }
