0bc8087444259f8e80dc5ab37d39bace5a975780
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-03 17:03:02 +0530

    Updated code

--- src/main/java/com/jsp/resturant_module/dto/ResturantRequest.java
+++ src/main/java/com/jsp/resturant_module/dto/ResturantRequest.java
@@ -1,4 +1,12 @@
 package com.jsp.resturant_module.dto;
 
+import lombok.Data;
+
 public class ResturantRequest {
+
+    @Data
+    private String resturantName;
+    private Long phone;
+    private String address;
+    private Integer pincode;
 }
--- src/main/java/com/jsp/resturant_module/service/ResturantService.java
+++ src/main/java/com/jsp/resturant_module/service/ResturantService.java
@@ -1,4 +1,31 @@
 package com.jsp.resturant_module.service;
 
-public class ResturantService {
+import com.jsp.resturant_module.dto.ResturantRequest;
+import com.jsp.user_module.model.dto.LoginRequest;
+import com.jsp.user_module.model.dto.UserRequest;
+import com.jsp.user_module.model.dto.UserResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ResturantService {
+
+    ResturantResponse createResturant(ResturantRequest resturantRequest);
+    ResturantResponse
+
+
+
+
+
+
+
+//    UserResponse register(UserRequest user);
+//    UserResponse login(LoginRequest login);
+//    UserResponse profile(Integer id);
+//    void deleteUser(Integer id);
+//    UserResponse changePassword(String email, String oldPassword, String newPassword);
+
+
+
+
+
 }
